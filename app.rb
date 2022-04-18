b0be9914de0ddb71adaed69ecdf65a5c675df0e3
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2022-04-18 13:41:25 -0600

    CREATE App CLASS

---
+++ app.rb
@@ -0,0 +1,24 @@
+class App
+def initialize
+  @books = []
+  @music_albums = []
+  @movies = []
+  @games = []
+  @genres = []
+  @labels = []
+  @authors = []
+  @sources = []
+end
+
+# List all books
+# List all music albums
+# List all movies
+# List of games
+# List all genres (e.g 'Comedy', 'Thriller')
+# List all labels (e.g. 'Gift', 'New')
+# List all authors (e.g. 'Stephen King')
+# List all sources (e.g. 'From a friend', 'Online shop')
+# Add a book
+# Add a music album
+# Add a movie
+# Add a game
