f14c78e6335e9ee50d2829624f3e54e4b72b0317
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2022-04-18 14:27:17 -0600

    RUN LINTERS

--- app.rb
+++ app.rb
@@ -10,16 +10,3 @@ class App
     @sources = []
   end
 end
-
-# List all books
-# List all music albums
-# List all movies
-# List of games
-# List all genres (e.g 'Comedy', 'Thriller')
-# List all labels (e.g. 'Gift', 'New')
-# List all authors (e.g. 'Stephen King')
-# List all sources (e.g. 'From a friend', 'Online shop')
-# Add a book
-# Add a music album
-# Add a movie
-# Add a game
