79d4348ef09d8fc841cef146ccd0cddb02deeaea
Bondok6 <kirolousbondok6@gmail.com>
2022-04-18 19:56:10 +0200

    Create can_be_archive? method

--- item.rb
+++ item.rb
@@ -1,26 +1,24 @@
+require('date')
+
 class Item
   attr_reader :id, :archived
-  attr_accessor :genre, :author, :source, :label, :publish_date
+  attr_accessor :publish_date
 
-  def initialize(id: Rand.random(1.1000), publish_date: 'Unknown', archived: false)
+  def initialize(id: Rand.random(1.1000), publish_date: Date.today.year, archived: false)
     @id = id
     @publish_date = publish_date
     @archived = archived
   end
 
   def can_be_archived?
+    current_year = Date.today.year
+    publish_year = Date.parse(@publish_date).year
+    current_year - publish_year > 10
   end
 
   def move_to_archive
+
   end
 
-  private can_be_archived?
+  private :can_be_archived?
 end
-
-
-# @genre = genre
-# @author = author
-# @source = source
-# @label = label
-
-# genre = 'Unknown', author = 'Unknown', source = 'Unknown', label = 'Unknown',
