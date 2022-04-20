39b5d6acef98b5536edaf17cb01abb029ba6e42f
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2022-04-20 14:51:12 -0600

    ADD list_all_books FUNC AND add_books SECRET FUNC

--- app.rb
+++ app.rb
@@ -1,3 +1,5 @@
+require './classes/book'
+
 class App
   def initialize
     @books = []
@@ -8,5 +10,20 @@ class App
     @labels = []
     @authors = []
     @sources = []
+    add_books
+  end
+
+  def add_books
+    temp_arr = ['Ohara good', 'Killer bad', 'Whale perfect', 'Hawaii dirty']
+    temp_arr.each do |str|
+      publisher, cover_state = str.split
+      @books << Book.new(publisher, cover_state)
+    end
+  end
+
+  def list_all_books
+    @books.each_with_index do |book, index|
+      puts "[#{index + 1}] Publisher: #{book.publisher}, Cover State: #{book.cover_state}"
+    end
   end
 end
