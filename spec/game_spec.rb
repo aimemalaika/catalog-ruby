ad9901c7d33856c3d55428df8ce6eb10ed0830fd
Bondok6 <kirolousbondok6@gmail.com>
2022-04-20 17:54:06 +0200

    Fix linters

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -11,7 +11,6 @@ describe 'Game' do
       game = Game.new(false, '22/2/2002', '2/2/2019')
       expect(game.multiplayer).to be false
     end
-
   end
 
   context 'when call can_be_archived? method' do
