c2e2cb67f924128b42ce91bd222e3842167a3f46
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2022-04-20 16:45:09 -0600

    IMPLEMENT add_book FUNCTIONALITY AT USER'S CONSOLE

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require './modules/book_module'
 require './modules/label_module'
+require './inputs/sanchito_inputs'
 
 class App
   include AddBooks
@@ -27,5 +28,22 @@ class App
     @labels.each_with_index do |label, index|
       puts "[#{index + 1}] Title: #{label.title}, Color: #{label.color}, Items: #{label.items}, ID: #{label.id}"
     end
+  end
+
+  def add_book
+    puts "Let's create a book!"
+    publisher, cover_state =  grap_book_data
+
+    puts "\nChoose a label from the following list using a number"
+    sleep(1)
+    list_all_labels
+    print "\nYour selected label: "
+    label_chosen = gets.chomp.to_i
+    new_book = Book.new(publisher, cover_state)
+    new_book.add_label(@labels[label_chosen - 1])
+    @books << new_book
+    puts "\nBook created successfully!"
+    puts "\n\n...Returning to main menu...\n\n"
+    sleep(1)
   end
 end
---
+++ inputs/sanchito_inputs.rb
@@ -0,0 +1,9 @@
+def grap_book_data
+  print "\nPlease enter the name of it's publisher: "
+  publisher = gets.chomp
+
+  print "\nPlease enter the state of it's cover [good or bad]: "
+  cover_state =  gets.chomp
+  
+  [publisher, cover_state]
+end
