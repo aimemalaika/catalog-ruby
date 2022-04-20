dc86b04e78dae2eabe9421e92c94477abefabc27
Bondok6 <kirolousbondok6@gmail.com>
2022-04-20 21:50:28 +0200

    implement option methods

--- app.rb
+++ app.rb
@@ -1,3 +1,7 @@
+require_relative 'classes/author'
+require_relative 'classes/game'
+require_relative 'inputs/inputs'
+
 class App
   def initialize
     @books = []
@@ -8,5 +12,38 @@ class App
     @labels = []
     @authors = []
     @sources = []
+    add_authors
   end
+
+  def add_authors
+    author_names = ['Stephen King', 'Marguerite Duras', 'Saul Bellow', 'Ernest Hemingway']
+    author_names.each do |author|
+      first_name, last_name = author.split(' ')
+      @authors << Author.new(first_name, last_name)
+    end
+  end
+
+  def list_all_authors
+    @authors.each_with_index do |author, index|
+      puts "#{index + 1 }) #{author.first_name} #{author.last_name}"
+    end
+  end
+
+  def add_game
+    multiplayer, last_played_at, publish_date = grap_game_data
+    puts 'Choose an author from the following list using the number'
+    list_all_authors
+    author_chosen = gets.chomp.to_i
+    new_game = Game.new(multiplayer, last_played_at, publish_date)
+    new_game.add_author(@authors[author_chosen - 1])
+    @games << new_game
+    puts 'The game has been added successfully!'
+  end
+
+  def list_all_games
+    @games.each_with_index do |game, index|
+      puts "#{index + 1}) Last Played: #{game.last_played_at}, Multiplayer: #{game.multiplayer}, publish: #{game.publish_date}"
+    end
+  end
+
 end
