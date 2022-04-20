12a1eb0c987898265dda31f73b568a5492f1b010
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2022-04-20 16:05:31 -0600

    ADD list_all_labels FUNC, MIRRORING list_all_books

--- app.rb
+++ app.rb
@@ -1,7 +1,9 @@
 require './modules/book_module'
+require './modules/label_module'
 
 class App
   include AddBooks
+  include AddLabels
   def initialize
     @books = []
     @music_albums = []
@@ -12,11 +14,18 @@ class App
     @authors = []
     @sources = []
     add_books(@books)
+    add_labels(@labels)
   end
 
   def list_all_books
     @books.each_with_index do |book, index|
       puts "[#{index + 1}] Publisher: #{book.publisher}, Cover State: #{book.cover_state}"
+    end
+  end
+
+  def list_all_labels
+    @labels.each_with_index do |label, index|
+      puts "[#{index + 1}] Title: #{label.title}, Color: #{label.color}, Items: #{label.items}, ID: #{label.id}"
     end
   end
 end
