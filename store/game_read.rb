5719cd2858b520cd297371172d67d7812d8f353e
Kyrillos Hany <kirolousbondok6@gmail.com>
2022-04-21 18:41:28 +0200

    Update store/game_read.rb
    Co-authored-by: eduardosancho <edsancho18@yahoo.es>

--- store/game_read.rb
+++ store/game_read.rb
@@ -3,7 +3,7 @@ require 'json'
 
 def read_games
   data = []
-  if File.exist?('games.json')
+  if File.exist?('data/games.json')
     games_data = JSON.parse(File.read('./data/games.json'))
     games_data.each do |game|
       data.push(Game.new(game['multiplayer'], game['last_played_at'], game['publish_date']))
